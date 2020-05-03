f6e5d0737cb0e16bf6f1207dc2ff83c18ef8716b
Samuel Caldas <samuel.caldas@gmail.com>
2020-05-03 01:47:19 -0300

    Adicionando comentários

--- main.py
+++ main.py
@@ -11,99 +11,121 @@ GAMMA = 0.95
 
 
 def main():
-    env = gym.make('CartPole-v0')
-    env.seed(0)
-    ob_space = env.observation_space
-    Policy = Policy_net('policy', env)
-    Old_Policy = Policy_net('old_policy', env)
-    PPO = PPOTrain(Policy, Old_Policy, gamma=GAMMA)
-    saver = tf.train.Saver()
+    env = gym.make('CartPole-v0')                   # Instacia o ambiente CartPole
+    env.seed(0)                                     #
+    ob_space = env.observation_space                # Descrevem o formato de observações válidas do espaço
+    Policy = Policy_net('policy', env)              # Cria a rede de Politica
+    Old_Policy = Policy_net('old_policy', env)      # Cria a rede de politica antiga
+    PPO = PPOTrain(Policy, Old_Policy, gamma=GAMMA) #
+    saver = tf.train.Saver()                        #
 
-    with tf.Session() as sess:
-        writer = tf.summary.FileWriter('./log/train', sess.graph)
-        sess.run(tf.global_variables_initializer()) # Inicializa as redes
+    with tf.Session() as sess:  # Bloco da sessão 
+        writer = tf.summary.FileWriter('./log/train', sess.graph)   # Define diretório de logs
+        sess.run(tf.global_variables_initializer())                 # Inicializa as redes
 
         obs = env.reset()   # Reseta o ambiente e obtem a primeira observaçao
         reward = 0          # Armazena as recompensas
-        success_num = 0
+        success_num = 0     # Contador de sucessos
 
-        for iteration in range(EPISODES):  # Loop do episodio
-            observations = []   # Array pra armazenar as observaçoes
-            actions = []        # Array pra armazenar as açoes
-            v_preds = []        # Array pra armazenar as previsoes
-            rewards = []        # Array pra armazenar as recompensas
-            run_policy_steps = 0# Contador de passos em cada epsodio
-            env.render()        # Renderiza o ambiente
-            while True:         # run policy RUN_POLICY_STEPS which is much less than episode length
-                run_policy_steps += 1   # Incrementa contador de passos de cada ep
-                obs = np.stack([obs]).astype(dtype=np.float32)  # prepare to feed placeholder Policy.obs
-                act, v_pred = Policy.act(obs=obs, stochastic=True)
+        for episode in range(EPISODES): # Loop do episodio
+            observations = []           # Array pra armazenar as observaçoes
+            actions = []                # Array pra armazenar as açoes
+            v_preds = []                # Array pra armazenar as previsoes
+            rewards = []                # Array pra armazenar as recompensas
+            run_policy_steps = 0        # Contador de passos em cada epsodio
+            env.render()                # Renderiza o ambiente
 
-                act     = act.item()
-                v_pred  = v_pred.item()
+            while True: # Run policy RUN_POLICY_STEPS which is much less than episode length
+                run_policy_steps += 1                               # Incrementa contador de passos de cada ep
+                obs = np.stack([obs]).astype(dtype=np.float32)      # prepare to feed placeholder Policy.obs
+                act, v_pred = Policy.act(obs=obs, stochastic=True)  # Corre a rede neural e obtem uma ação e o V previsto
 
-                observations.append(obs)
-                actions.append(act)
-                v_preds.append(v_pred)
-                rewards.append(reward)
+                act     = act.item()        # Transforma um array do numpy 
+                v_pred  = v_pred.item()     # em um objeto scalar do Python
 
-                next_obs, reward, done, info = env.step(act)
+                observations.append(obs)    # Adiciona a observação ao buffer de observações
+                actions.append(act)         # Adiciona a ação ao buffer de ações
+                v_preds.append(v_pred)      # Adiciona a v_pred ao buffer de v_pred
+                rewards.append(reward)      # Adiciona a recompensa ao buffer de recompensa
 
-                if done:
+                next_obs, reward, done, info = env.step(act)    # envia a ação ao ambiente e recebe a proxima observação, a recompensa e se o passo terminou
+
+                if done:                # Se o done for verdadero ...
                     v_preds_next = v_preds[1:] + [0]  # next state of terminate state has 0 state value
-                    obs = env.reset()
-                    reward = -1
-                    break
-                else:
-                    obs = next_obs
+                    obs = env.reset()   #   Redefine o ambiente
+                    reward = -1         #   Subtrai 1 da recompensa (?)
+                    break               #   Sai do loop while
+                else:                   # Senao...
+                    obs = next_obs      #   Armazena em obs a proxima observação
 
-            writer.add_summary(tf.Summary(value=[tf.Summary.Value(tag='episode_length', simple_value=run_policy_steps)])
-                               , iteration)
-            writer.add_summary(tf.Summary(value=[tf.Summary.Value(tag='episode_reward', simple_value=sum(rewards))])
-                               , iteration)
+            # Armazena em log para vizualização no tensorboard
+            writer.add_summary( 
+                tf.Summary(
+                    value=[
+                        tf.Summary.Value(
+                            tag='episode_length',           # Duração do episódio
+                            simple_value=run_policy_steps   # Contador de passos
+                        )
+                    ]
+                ),   
+                episode # Contador de eps
+            )
+            writer.add_summary(
+                tf.Summary(
+                    value=[
+                        tf.Summary.Value(
+                            tag='episode_reward',       # Recompensa do ep
+                            simple_value=sum(rewards)   # soma de todas as recompensas do ep
+                        )
+                    ]
+                ),       
+                episode # Contador de eps
+            )
 
-            # end condition of test
-            if sum(rewards) >= 195:
-                success_num += 1
-                if success_num >= 100:
-                    saver.save(sess, './model/model.ckpt')
-                    print('Clear!! Model saved.')
-                    break
-            else:
-                success_num = 0
+            # Condicional para finalizar o teste
+            if sum(rewards) >= 195:                         # Se a soma das recompensas for maior ou igual 195
+                success_num += 1                            #   Incrementa um no contador de sucessos
+                if success_num >= 100:                      #   Se ocorrerem 100 sucessos
+                    saver.save(sess, './model/model.ckpt')  #       Salve a sessão
+                    print('Clear!! Model saved.')           # 
+                    break                                   #       Saia do loop
+            else:                                           # senao, 
+                success_num = 0                             #   zera o contador de sucessos
 
-            gaes = PPO.get_gaes(rewards=rewards, v_preds=v_preds, v_preds_next=v_preds_next)
+            gaes = PPO.get_gaes(rewards=rewards, v_preds=v_preds, v_preds_next=v_preds_next) # ?
 
-            # convert list to numpy array for feeding tf.placeholder
-            observations = np.reshape(observations, newshape=[-1] + list(ob_space.shape))
-            actions = np.array(actions).astype(dtype=np.int32)
-            rewards = np.array(rewards).astype(dtype=np.float32)
-            v_preds_next = np.array(v_preds_next).astype(dtype=np.float32)
-            gaes = np.array(gaes).astype(dtype=np.float32)
-            gaes = (gaes - gaes.mean()) / gaes.std()
+            # Converte lista em NPArray para alimentar o tf.placeholder
+            observations    = np.reshape(observations, newshape=[-1] + list(ob_space.shape))
+            actions         = np.array(actions).astype(dtype=np.int32)
+            rewards         = np.array(rewards).astype(dtype=np.float32)
+            v_preds_next    = np.array(v_preds_next).astype(dtype=np.float32)
+            gaes            = np.array(gaes).astype(dtype=np.float32)
+            gaes            = (gaes - gaes.mean()) / gaes.std()
 
             PPO.assign_policy_parameters()
 
-            inp = [observations, actions, rewards, v_preds_next, gaes]
+            inp = [observations, actions, rewards, v_preds_next, gaes]  # Cria um array com 5 colunas: onservações, ações, recompensas, 
 
-            # train
+            # Treina
             for epoch in range(4):
                 sample_indices = np.random.randint(low=0, high=observations.shape[0], size=64)  # indices's are in [low, high)
-                sampled_inp = [np.take(a=a, indices=sample_indices, axis=0) for a in inp]  # sample training data
+                sampled_inp = [np.take(a=a, indices=sample_indices, axis=0) for a in inp]       # sample training data
                 PPO.train(obs=sampled_inp[0],
                     actions=sampled_inp[1],
                     rewards=sampled_inp[2],
                     v_preds_next=sampled_inp[3],
                     gaes=sampled_inp[4])
 
-            summary = PPO.get_summary(obs=inp[0],
+            summary = PPO.get_summary(
+                obs=inp[0],
                 actions=inp[1],
                 rewards=inp[2],
                 v_preds_next=inp[3],
-                gaes=inp[4])[0]
+                gaes=inp[4]
+            )[0]
 
-            writer.add_summary(summary, iteration)
-        writer.close()
+            writer.add_summary(summary, episode)
+        writer.close()  # Final do ep
 
 
 if __name__ == '__main__':
